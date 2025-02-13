8c5e7849c165332424d8cd234252f42d1e0d2dd2
kokolo4442 <159828248+kokolo4442@users.noreply.github.com>
2025-02-13 22:32:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Jesteś pewna?",
+    "Tak naprawde pewna",
+    "Na sto procent?",
+    "No dawaj",
+    "Pomyśl o tym!",
+    "Tak naprawde pomyśl",
+    "Jesteś pewna na milion procent?",
+    "Dobra przestane",
+    "Żartowałem, no dawaj!❤️",
+    "❤️"
 ];
 
 let messageIndex = 0;
